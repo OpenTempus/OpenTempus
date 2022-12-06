f6443c764c01a31d7e963af0dc31e6f523a1efe4
Daniel Krook <krook@us.ibm.com>
2022-12-05 22:00:01 -0500

    Fix text.
    Signed-off-by: Daniel Krook <krook@us.ibm.com>

--- src/component/about.js
+++ src/component/about.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import style from './home.module.css'
+import React from "react";
+import style from "./home.module.css";
 
 const About = () => {
-    return (
-        <div>
-            <div className={style.main_third_section}>
-            <div className={style.third_section}>
-                <div className={style.third_section_text}>
-                    <h2>what and why Opentemper?</h2>
-                    <h1></h1>
-                    <p className={style.third_section_first_text}>Climate change is not a new, and farmers are the one who are impacted the first, OpenTemper useage machine learning, gather more than five years of data information to prodict and server information to the farmers.  </p>
-                    <p className={style.third_section_second_text}> by useage the thouse information not only giving farmer straight forward weather data to be prepare but also cluclute the crop reqirement for farmer to fellow </p>
-                </div>
-                <div className={style.third_section_link}>
-                    <iframe height="350" width="500" src="https://www.youtube.com/embed/VQtx4Yq4UnE" frameborder="0"></iframe>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={style.main_third_section}>
+        <div className={style.third_section}>
+          <div className={style.third_section_text}>
+            <h2>what and why Opentemper?</h2>
+            <h1></h1>
+            <p className={style.third_section_first_text}>
+              Climate change is not a new, and farmers are the ones who are
+              impacted the most, OpenTempus uses machine learning, gathering
+              more than five years of data information to predict and serve
+              information to farmers.{" "}
+            </p>
+            <p className={style.third_section_second_text}>
+              {" "}
+              by usage the information not only gives farmer straightforward
+              weather data to prepare but also includes the crop requirement for
+              farmer to follow{" "}
+            </p>
+          </div>
+          <div className={style.third_section_link}>
+            <iframe
+              height="350"
+              width="500"
+              src="https://www.youtube.com/embed/VQtx4Yq4UnE"
+              frameborder="0"
+            ></iframe>
+          </div>
         </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/component/header.js
+++ src/component/header.js
@@ -1,65 +1,82 @@
-import React from 'react'
-import style from './home.module.css'
-import background from './midia/heading.jpg'
-import logo from './image/OpenTempusLogo.png'
-import githublogo from './image/github_logo.png'
-import LFlogo from './image/tlf_logo.png'
-import callforcode from './image/cr_logo.png'
+import React from "react";
+import style from "./home.module.css";
+import background from "./midia/heading.jpg";
+import logo from "./image/OpenTempusLogo.png";
+import githublogo from "./image/github_logo.png";
+import LFlogo from "./image/tlf_logo.png";
+import callforcode from "./image/cr_logo.png";
 
 const Header = () => {
   return (
-   <div className={style.backgroundImage} style={{ backgroundImage: `url(${background})` }}>
-    <div className={style.second_header}>
-       <div className={style.second_header_section}>
-            <div className={style.second_header_logo}>
-               <img src={logo} alt=""/>
+    <div
+      className={style.backgroundImage}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className={style.second_header}>
+        <div className={style.second_header_section}>
+          <div className={style.second_header_logo}>
+            <img src={logo} alt="" />
+          </div>
+          <div className={style.second_header_text}>
+            <p>
+              OpenTempus is an open source solution using machine learnig model
+              to prodict weather information <br /> and suporting farmer to
+              fight climate change. This code can be used to to run climate
+              prodiction <br /> according to your longitude and latitude
+            </p>
+          </div>
+          <div className={style.second_header_buttons}>
+            <div className={style.second_header_all_buttons}>
+              <a
+                href="https://github.com/Agrolly-LLC/Agrolly-Data-Science"
+                className={style.first_button}
+              >
+                <div>
+                  <img src={githublogo} alt="" />
+                </div>
+                <div>
+                  <p>OpenTempus code</p>
+                </div>
+              </a>
+              <a
+                href="http://agrolly-web.mybluemix.net/"
+                className={style.second_button}
+              >
+                <div>
+                  <img src="" alt="" />
+                </div>
+                <div>
+                  <p>Agrolly</p>
+                </div>
+              </a>
+              <a
+                href="https://www.linuxfoundation.org/projects/call-for-code/"
+                className={style.third_button}
+              >
+                <div>
+                  <img src={LFlogo} alt="" />
+                </div>
+                <div>
+                  <p>The Linux Foundation</p>
+                </div>
+              </a>
+              <a
+                href="https://developer.ibm.com/callforcode/"
+                className={style.fourth_button}
+              >
+                <div>
+                  <img src={callforcode} alt="" />
+                </div>
+                <div>
+                  <p>Call for Code</p>
+                </div>
+              </a>
             </div>
-            <div className={style.second_header_text}>
-                <p>OpenTempus is an open source solution using machine learnig model to prodict weather information <br/> and suporting farmer to fight climate change. This code can be used to to run climate prodiction <br/> according to your longitude and latitude</p>
-            </div>
-            <div className={style.second_header_text2}>
-                <p>—— HELPFUL LINKS ——</p>
-            </div>
-            <div className={style.second_header_buttons}>
-               <div className={style.second_header_all_buttons}>
-                    <a href='https://github.com/Agrolly-LLC/Agrolly-Data-Science' className={style.first_button}>
-                        <div>
-                          <img src={githublogo} alt=""/>
-                        </div>
-                        <div>
-                          <p>Protocol Code</p>
-                        </div> 
-                    </a>
-                    <a href='http://agrolly-web.mybluemix.net/' className={style.second_button}>
-                        <div>
-                          <img src="" alt=""/>
-                        </div>
-                        <div>
-                          <p>Agrolly</p>
-                        </div> 
-                    </a>
-                    <a href='https://www.linuxfoundation.org/projects/call-for-code/' className={style.third_button}>
-                        <div>
-                          <img src={LFlogo} alt=""/>
-                        </div>
-                        <div>
-                          <p>The Linux Foundation</p>
-                        </div> 
-                    </a>
-                    <a href='https://developer.ibm.com/callforcode/' className={style.fourth_button}>
-                        <div>
-                          <img src={callforcode} alt=""/>
-                        </div>
-                        <div>
-                          <p>Call For Code</p>
-                        </div> 
-                    </a>
-               </div>
-            </div>
-       </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
